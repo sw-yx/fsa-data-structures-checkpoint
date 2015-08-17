63110a9e4fce7994f7ed77f626c78e87411c2005
Gabriel Lebec <glebec@gmail.com>
2015-08-16 22:10:32 -0400

    underflow tests for queue/stack

--- data.spec.js
+++ data.spec.js
@@ -17,12 +17,17 @@ describe('A queue', function(){
     expect(queue.remove()).toBe(uniqueObj);
   });
 
+  it('returns `undefined` on underflow (empty)', function(){
+    expect(queue.remove()).toBe(undefined);
+  });
+
   // FIFO: First In, First Out
   it('adds and removes three items in a FIFO way', function(){
     queue.add(5).add(uniqueObj).add('fullstack');
     expect(queue.remove()).toBe(5);
     expect(queue.remove()).toBe(uniqueObj);
     expect(queue.remove()).toBe('fullstack');
+    expect(queue.remove()).toBe(undefined);
   });
 
   it('can handle interspersed add and remove', function(){
@@ -33,6 +38,7 @@ describe('A queue', function(){
     queue.add(4);
     expect(queue.remove()).toBe(3);
     expect(queue.remove()).toBe(4);
+    expect(queue.remove()).toBe(undefined);
   });
 
   // no globals!
@@ -41,7 +47,9 @@ describe('A queue', function(){
     queue.add('fullstack');
     q2.add('JavaScript');
     expect(q2.remove()).toBe('JavaScript');
+    expect(q2.remove()).toBe(undefined);
     expect(queue.remove()).toBe('fullstack');
+    expect(queue.remove()).toBe(undefined);
   });
 
 });
@@ -58,12 +66,17 @@ describe('A stack', function(){
     expect(stack.remove()).toBe(uniqueObj);
   });
 
+  it('returns `undefined` on underflow (empty)', function(){
+    expect(stack.remove()).toBe(undefined);
+  });
+
   // LIFO: Last In, First Out
   it('adds and removes three items in a LIFO way', function(){
     stack.add(5).add(uniqueObj).add('fullstack');
     expect(stack.remove()).toBe('fullstack');
     expect(stack.remove()).toBe(uniqueObj);
     expect(stack.remove()).toBe(5);
+    expect(stack.remove()).toBe(undefined);
   });
 
   it('can handle interspersed add and remove', function(){
@@ -74,6 +87,7 @@ describe('A stack', function(){
     stack.add(4);
     expect(stack.remove()).toBe(4);
     expect(stack.remove()).toBe(2);
+    expect(stack.remove()).toBe(undefined);
   });
 
   // no globals!
@@ -82,7 +96,9 @@ describe('A stack', function(){
     stack.add('fullstack');
     s2.add('JavaScript');
     expect(stack.remove()).toBe('fullstack');
+    expect(stack.remove()).toBe(undefined);
     expect(s2.remove()).toBe('JavaScript');
+    expect(s2.remove()).toBe(undefined);
   });
 
 });
