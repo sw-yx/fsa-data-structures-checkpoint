08bb6e65c2ebfdb1a5e9653631d900ade3644ec0
sw-yx <shawnthe1@gmail.com>
2017-07-26 18:42:23 -0400

    done

--- data.js
+++ data.js
@@ -10,15 +10,18 @@ will not need to edit the lines above and below them.
 
 function Stack () {
   // your code here
+  this.arr = []
 }
 
 Stack.prototype.add = function (item) {
   // your code here
+  this.arr.push(item)
   return this; // for chaining, do not edit
 };
 
 Stack.prototype.remove = function () {
   // your code here
+  return this.arr.pop()
 };
 
 //-----------------------------------------
@@ -55,12 +58,29 @@ function ListNode (item, prev, next) {
 }
 
 LinkedList.prototype.addToTail = function (item) {
+
+	var oldTail = this.tail
+	this.tail = new Node(x, null, oldTail)
+	if (oldTail) oldTail.next = this.tail
+	if (!this.head) this.head = this.tail
   // your code here
   return this; // for chaining, do not edit
 };
 
 LinkedList.prototype.removeFromTail = function () {
   // your code here
+
+	if (!this.tail) {
+		return null
+	} else {
+		// this.tail points to this.tail.next
+		var oldTail = this.tail
+		this.tail = this.tail.previous
+		if (this.tail) this.tail.next = null
+		// and return this.tail.next
+		if (!this.tail) this.head = null
+		return oldTail.value
+	}
 };
 
 LinkedList.prototype.forEach = function (iterator) {
@@ -72,6 +92,7 @@ LinkedList.prototype.forEach = function (iterator) {
 
 function Alist () {
   // your code here
+  this.head = null
 }
 
 function AlistNode (key, value, next) {
@@ -82,11 +103,30 @@ function AlistNode (key, value, next) {
 
 Alist.prototype.set = function (key, value) {
   // your code here
+  // console.log('this', this, 'key', key, 'value', value)
+  var ptr = this.head
+  // while (ptr) {
+  //   console.log('ptr', ptr)
+  //   if (ptr && ptr.key === key) {
+  //     // set new value
+  //     ptr.value = value
+  //     return this
+  //   } 
+  //   ptr = ptr.next
+  // }
+  this.head = new AlistNode(key,value, ptr)
   return this; // for chaining; do not edit
 };
 
 Alist.prototype.get = function (key) {
   // your code here
+  var ptr = this.head
+  while (ptr) {
+    if (ptr.key === key) {
+      return ptr.value
+    }
+    ptr = ptr.next
+  }
 };
 
 
@@ -104,15 +144,22 @@ function hash (key) {
 function HashTable () {
   this.buckets = Array(20);
   // your code here
+  var x = []
+  for (var i = 0; i< 20; i++){
+    x.push(new Alist())
+  }
+  this.buckets = x
 }
 
 HashTable.prototype.set = function (key, value) {
   // your code here. DO NOT simply set a prop. on an obj., that is cheating.
+  this.buckets[hash(key)].set(key,value)
   return this; // for chaining, do not edit
 };
 
 HashTable.prototype.get = function (key) {
   // your code here. DO NOT simply get a prop. from an obj., that is cheating.
+  return this.buckets[hash(key)].get(key)
 };
 
 //-----------------------------------------
@@ -120,25 +167,63 @@ HashTable.prototype.get = function (key) {
 
 function BinarySearchTree (val) {
   // your code here
+	this.value = val
+	this.left = null
+	this.right = null
 }
 
-BinarySearchTree.prototype.insert = function (val) {
+BinarySearchTree.prototype.insert = function (v) {
   // your code here
+  if (this.value < v) {
+      if (this.right) {
+          this.right.insert(v) 
+      } else {
+          this.right = new BinarySearchTree(v)
+      }
+  } else {
+      if (this.left) {
+          this.left.insert(v) 
+      } else {
+          this.left = new BinarySearchTree(v)
+      }
+  }
   return this; // for chaining, do not edit
 };
 
 BinarySearchTree.prototype.min = function () {
   // your code here
+  return this.left ? this.left.min() : this.value
 };
 
 BinarySearchTree.prototype.max = function () {
   // your code here
+  return this.right ? this.right.max() : this.value
 };
 
-BinarySearchTree.prototype.contains = function (val) {
+BinarySearchTree.prototype.contains = function (v) {
   // your code here
+    if (this.value == v) {
+        return true
+    } else {
+        if (this.right) {
+            var rightside = this.right.contains(v) 
+        } else {
+            var rightside = false
+        }
+        if (this.left) {
+            var leftside = this.left.contains(v) 
+        } else {
+            var leftside = false
+        }
+        return leftside || rightside
+    }
 };
 
-BinarySearchTree.prototype.traverse = function (iterator) {
+BinarySearchTree.prototype.traverse = function (fn) {
   // your code here
+  if (this.left) this.left.traverse(fn);
+  // visit the root
+  fn(this.value)
+  // Inorder right subtree
+  if (this.right) this.right.traverse(fn);
 };
